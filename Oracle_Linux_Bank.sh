575cee6075de4d64730d35b65522c4beb7b557c2
kira7z <zekia7@users.noreply.github.com>
2025-09-02 08:22:16 -0400

    Update Oracle_Linux_Bank.sh

--- Oracle_Linux_Bank.sh
+++ Oracle_Linux_Bank.sh
@@ -103,29 +103,6 @@ for user in $(cut -d: -f1 /etc/passwd); do
     passwd -S $user 2>/dev/null
 done
 
-#19. Estados de los Usuarios - Estatus
-# usuarios_shadow_estado.sh
-# Muestra usuario, UID, valor de shadow y estatus
-
-printf "%-20s %-6s %-20s %-15s\n" "USUARIO" "UID" "SHADOW($2)" "ESTATUS"
-echo "-----------------------------------------------------------------------"
-
-while IFS=: read -r user x uid gid gecos home shell; do
-  shadow2=$(awk -F: -v u="$user" '$1==u{print $2}' /etc/shadow 2>/dev/null)
-
-  if [[ "$shadow2" =~ ^\$ ]]; then
-    estatus="ACTIVO (con contraseña)"
-  elif [[ "$shadow2" == "!" || "$shadow2" == "!!" || "$shadow2" == "*" ]]; then
-    estatus="INACTIVO (bloqueado)"
-  elif [[ -z "$shadow2" ]]; then
-    estatus="INACTIVO (sin contraseña)"
-  else
-    estatus="OTRO: $shadow2"
-  fi
-
-  printf "%-20s %-6s %-20s %-15s\n" "$user" "$uid" "${shadow2:-N/A}" "$estatus"
-done < /etc/passwd
-
 echo -e "\n========================================"
 echo " FIN DE AUDITORÍA "
 echo "========================================"
