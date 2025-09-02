42975f3a3037fe80fbbeed67d2a2747bcbe38319
kira7z <zekia7@users.noreply.github.com>
2025-09-02 08:19:57 -0400

    Update Oracle_Linux_Bank.sh

--- Oracle_Linux_Bank.sh
+++ Oracle_Linux_Bank.sh
@@ -106,27 +106,28 @@ done
 #19. Estados de los Usuarios - Estatus
 # usuarios_shadow_estado.sh
 # Muestra usuario, UID, valor de shadow y estatus
- 
+
 printf "%-20s %-6s %-20s %-15s\n" "USUARIO" "UID" "SHADOW($2)" "ESTATUS"
 echo "-----------------------------------------------------------------------"
- 
-while IFS=: read -r user _ uid _ _ _ _; do
-  shadow2=$(sudo awk -F: -v u="$user" '$1==u{print $2}' /etc/shadow 2>/dev/null)
- 
+
+while IFS=: read -r user x uid gid gecos home shell; do
+  shadow2=$(awk -F: -v u="$user" '$1==u{print $2}' /etc/shadow 2>/dev/null)
+
   if [[ "$shadow2" =~ ^\$ ]]; then
-    estatus="ACTIVO"
-  elif [[ -z "$shadow2" || "$shadow2" == "!" || "$shadow2" == "!!" || "$shadow2" == "*" ]]; then
-    estatus="INACTIVO"
+    estatus="ACTIVO (con contraseña)"
+  elif [[ "$shadow2" == "!" || "$shadow2" == "!!" || "$shadow2" == "*" ]]; then
+    estatus="INACTIVO (bloqueado)"
+  elif [[ -z "$shadow2" ]]; then
+    estatus="INACTIVO (sin contraseña)"
   else
     estatus="OTRO: $shadow2"
   fi
- 
+
   printf "%-20s %-6s %-20s %-15s\n" "$user" "$uid" "${shadow2:-N/A}" "$estatus"
 done < /etc/passwd
 
 echo -e "\n========================================"
 echo " FIN DE AUDITORÍA "
 echo "========================================"
-
 
 -----------------------------------------------------------------------
